e73837527fe65183ea1dcbc9c4573dfb7222c1c5
Michael Memory <michaelmemoria@gmail.com>
2015-04-24 16:45:04 -0600

    Completed file structure and got Grunt working with Sass and compass

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,43 @@
 module.exports = function(grunt) {
 
-    grunt.registerTask('', function() {
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-compass');
 
-    })
+    grunt.initConfig({
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['src/js/**/*.js'],
+                dest: 'build/js/scripts.js'
+            }
+        },
+        compass: {
+            dev: {
+                options: {
+                    config: 'config.rb'
+                }
+            }
+        },
+        watch: {
+            options: {
+                livereload: true
+            },
+            scss: {
+                files: ['src/styles/styles.scss', 'src/styles/partials/*.scss'],
+                tasks: ['compass:dev']
+            },
+            js: {
+                files: 'src/js/**/*.js',
+                tasks: ['concat']
+            },
+            html: {
+                files: ['*.html']
+            }
+        }
+    });
 
+    grunt.registerTask('default', 'watch');
 };
