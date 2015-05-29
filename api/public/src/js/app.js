ec50e4635138712b485d44eafe2dea7734f0cc4f
Michael Memory <michaelmemoria@gmail.com>
2015-05-29 14:51:14 -0600

    undid a lot of things, dashboard.html seemed to make it break

--- api/public/src/js/app.js
+++ api/public/src/js/app.js
@@ -1,33 +1,37 @@
 var app = angular.module('trinkApp', ['ui.router', 'ngAnimate']);
 
+app.constant('CONSTANT', {
+    // API server
+    url: 'http://localhost:3000/'
+});
+
 app.config(function($stateProvider, $urlRouterProvider) {
 
     $stateProvider
         .state('login', {
             url: '/login',
-            templateUrl: './src/templates/loginHome.html',
+            templateUrl: 'src/templates/loginHome.html',
             controller: 'loginControl'
         })
         .state('dashboard', {
             abstract: true,
             url: '/dashboard/:id',
-            templateUrl: './src/templates/dashboard.html',
+            templateUrl: 'src/templates/dashboard.html',
             controller: 'dashControl',
             resolve: {
                 getUser: function(MainService, $stateParams) {
-                    console.log('got to resolve in app.js');
                     return MainService.getCurrentUser($stateParams.id);
                 }
             }
         })
         .state('dashboard.all-items', {
             url: '/trinkets',
-            templateUrl: './src/templates/allTrinkets.html',
+            templateUrl: 'src/templates/allTrinkets.html',
             controller: 'dashControl'
         })
         .state('dashboard.my-items', {
             url: '/mytrinkets',
-            templateUrl: './src/templates/userTrinkets.html',
+            templateUrl: 'src/templates/userTrinkets.html',
             controller: 'dashControl'
         });
 
